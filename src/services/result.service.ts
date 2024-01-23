c634347e09462857c18e6b1c22a586113327a119
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-01-23 15:49:20 +0100

    SCP-55 Adds extra info into licenses output

--- src/services/result.service.ts
+++ src/services/result.service.ts
@@ -6,27 +6,47 @@ export async function readResult(filepath: string): Promise<ScannerResults> {
   return JSON.parse(content) as ScannerResults
 }
 
-export function getLicenses(results: ScannerResults): string[] {
-  const licenses = new Set<string>()
+export interface Licenses {
+  spdxid: string
+  copyleft: boolean | null
+  url: string | null
+}
+
+export function getLicenses(results: ScannerResults): Licenses[] {
+  const licenses = new Array<Licenses>()
 
   for (const component of Object.values(results)) {
     for (const c of component) {
+      if (c.id === ComponentID.FILE || c.id === ComponentID.SNIPPET) {
+        for (const l of (c as ScannerComponent).licenses) {
+          licenses.push({
+            spdxid: l.name,
+            copyleft: !l.copyleft ? null : l.copyleft === 'yes' ? true : false,
+            url: l?.url ? l.url : null
+          })
+        }
+      }
+
       if (c.id === ComponentID.DEPENDENCY) {
         const dependencies = (c as DependencyComponent).dependencies
         for (const d of dependencies) {
           for (const l of d.licenses) {
-            licenses.add(l.spdx_id)
+            if (!l.spdx_id) continue
+            licenses.push({ spdxid: l.spdx_id, copyleft: null, url: null })
           }
-        }
-      }
-
-      if (c.id === ComponentID.FILE || c.id === ComponentID.SNIPPET) {
-        for (const l of (c as ScannerComponent).licenses) {
-          licenses.add(l.name)
         }
       }
     }
   }
 
-  return Array.from(licenses)
+  const seenSpdxIds = new Set<string>()
+  const uniqueLicenses = licenses.filter(license => {
+    if (!seenSpdxIds.has(license.spdxid)) {
+      seenSpdxIds.add(license.spdxid)
+      return true
+    }
+    return false
+  })
+
+  return uniqueLicenses
 }
