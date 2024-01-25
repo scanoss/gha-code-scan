e7807005510713e5f97b9d33dcc30423aa50b5db
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-01-25 16:30:14 +0100

    SCP-79 Adds parameter 'with-dependencies'

--- src/app.input.ts
+++ src/app.input.ts
@@ -6,3 +6,4 @@ export const SBOM_INDENTIFY = core.getInput('sbom-identify');
 export const SBOM_IGNORE = core.getInput('sbom-ignore');
 export const API_KEY = core.getInput('api-key');
 export const API_URL = core.getInput('api-url');
+export const WITH_DEPENDENCIES = core.getInput('with-dependencies');
