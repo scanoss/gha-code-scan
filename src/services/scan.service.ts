cbed92d303f0b24b57d436bd4463be7301a563b3
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-01-29 18:34:46 +0100

    SCP-87 upload artifact with scan results

--- src/services/scan.service.ts
+++ src/services/scan.service.ts
@@ -1,4 +1,11 @@
+import path from 'path';
 import * as input from '../app.input';
+import { DefaultArtifactClient } from '@actions/artifact';
+const artifact = new DefaultArtifactClient();
+
+export async function uploadResults(): Promise<void> {
+  await artifact.uploadArtifact(path.basename(input.OUTPUT_PATH), [input.OUTPUT_PATH], path.dirname(input.OUTPUT_PATH));
+}
 
 export function commandBuilder(): string {
   return `docker run -v "${input.REPO_DIR}":"/scanoss" ghcr.io/scanoss/scanoss-py:v1.9.0 scan . 
