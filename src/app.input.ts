6d649dd6ef5e28dce4a3a92aab1c26bf01f46921
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-01-30 17:02:32 +0100

    SCP-104 Redefine input config

--- src/app.input.ts
+++ src/app.input.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core';
 
+export const POLICIES = core.getInput('policies');
+export const SBOM_ENABLED = core.getInput('sbom.enabled') === 'true';
+export const SBOM_FILEPATH = core.getInput('sbom.filepath');
+export const SBOM_TYPE = core.getInput('sbom.type');
+export const DEPENDENCIES_ENABLED = core.getInput('dependencies.enabled') === 'true';
+export const API_KEY = core.getInput('api.key');
+export const API_URL = core.getInput('api.url');
+export const OUTPUT_FILEPATH = core.getInput('output.filepath');
+export const GITHUB_TOKEN = core.getInput('github.token');
 export const REPO_DIR = process.env.GITHUB_WORKSPACE as string;
-export const GITHUB_TOKEN = core.getInput('github-token');
-export const OUTPUT_PATH = core.getInput('output-path');
-export const SBOM_INDENTIFY = core.getInput('sbom-identify');
-export const SBOM_IGNORE = core.getInput('sbom-ignore');
-export const API_KEY = core.getInput('api-key');
-export const API_URL = core.getInput('api-url');
-export const WITH_DEPENDENCIES = core.getInput('with-dependencies');
