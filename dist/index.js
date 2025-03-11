d078b1ee3af5c4b6b121604aac832e56191286ef
Agustin Groh <agusgroh@gmail.com>
2025-03-11 10:24:40 -0300

    bug:SP-2119 Fixes bug on API key

--- dist/index.js
+++ dist/index.js
@@ -125796,7 +125796,7 @@ exports.COPYLEFT_LICENSE_INCLUDE = core.getInput('licenses.copyleft.include');
 exports.COPYLEFT_LICENSE_EXCLUDE = core.getInput('licenses.copyleft.exclude');
 exports.COPYLEFT_LICENSE_EXPLICIT = core.getInput('licenses.copyleft.explicit');
 exports.REPO_DIR = process.env.GITHUB_WORKSPACE;
-exports.RUNTIME_CONTAINER = core.getInput('runtimeContainer') || 'ghcr.io/scanoss/scanoss-py:v1.19.0';
+exports.RUNTIME_CONTAINER = core.getInput('runtimeContainer') || 'ghcr.io/scanoss/scanoss-py:v1.20.4';
 exports.SKIP_SNIPPETS = core.getInput('skipSnippets') === 'true';
 exports.SCAN_FILES = core.getInput('scanFiles') === 'true';
 exports.SCANOSS_SETTINGS = core.getInput('scanossSettings') === 'true';
@@ -127176,7 +127176,7 @@ class ScanService {
             ...(await this.detectSBOM()),
             ...this.buildSnippetArgs(),
             ...(this.options.apiUrl ? ['--apiurl', this.options.apiUrl] : []),
-            ...(this.options.apiKey ? ['--apiKey', this.options.apiKey.replace(/\n/gm, ' ')] : [])
+            ...(this.options.apiKey ? ['--key', this.options.apiKey.replace(/\n/gm, ' ')] : [])
         ];
     }
     /**
