0eec2273c29f33379c22090b1d64c33289ef5261
Agustin Groh <agusgroh@gmail.com>
2025-06-12 11:48:00 -0300

    chore:SP-2676 Upgrades scanoss-py version to v1.25.1

--- src/app.input.ts
+++ src/app.input.ts
@@ -37,7 +37,7 @@ export const COPYLEFT_LICENSE_INCLUDE = core.getInput('licenses.copyleft.include
 export const COPYLEFT_LICENSE_EXCLUDE = core.getInput('licenses.copyleft.exclude');
 export const COPYLEFT_LICENSE_EXPLICIT = core.getInput('licenses.copyleft.explicit');
 export const REPO_DIR = process.env.GITHUB_WORKSPACE as string;
-export const RUNTIME_CONTAINER = core.getInput('runtimeContainer') || 'ghcr.io/scanoss/scanoss-py:v1.20.4';
+export const RUNTIME_CONTAINER = core.getInput('runtimeContainer') || 'ghcr.io/scanoss/scanoss-py:v1.25.1';
 export const SKIP_SNIPPETS = core.getInput('skipSnippets') === 'true';
 export const SCAN_FILES = core.getInput('scanFiles') === 'true';
 export const SCANOSS_SETTINGS = core.getInput('scanossSettings') === 'true';
